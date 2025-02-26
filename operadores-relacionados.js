9be419acd57bd9e49d7c99f5c6d192a74bd70efd
B99Dev <brunom1.dev@gmail.com>
2025-02-26 13:54:41 -0300

    Removido os comentarios do fonte

--- operadores-relacionados.js
+++ operadores-relacionados.js
@@ -1,16 +1,3 @@
-/*   nota1, nota2: real
-
-Inicio
-// Seção de Comandos, procedimento, funções, operadores, etc... 
-   nota1 := 10
-   nota2 := 15
-   
-   se(nota1 <> nota2)entao
-      escreval("Nota 1 é diferente")
-   senao
-      escreva("Nota 1 é igual")
-      fimse
-      */
 
       var nota1, nota2
       nota1 = 10
